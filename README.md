e2a845c303b500b3770fdd20ee221d7219741e44
Or Rikon <rikonor@gmail.com>
2016-06-30 15:42:08 -0400

    added env-config, added readme with usage instructions

---
+++ README.md
@@ -0,0 +1,35 @@
+env-config
+---
+
+Share environment variables between server and client.
+
+### Usage
+
+On server
+```
+import envConfig from 'env-config';
+
+// Register desired env vars and their default values
+envConfig.registerEnvVar("ORIGIN", "https://thenib.com");
+
+// Extract the desired values from the environment
+envConfig.extractVarsFromEnv();
+```
+
+Then when rendering the index page on the server, include the output of `envConfig.renderScriptTag()`.
+```
+app.use('*', (req, res) => {
+  // This renders an index page template
+  res.render('index', {
+    envConfigHtml: envConfig.renderScriptTag(),
+  });
+});
+
+```
+
+On client
+```
+import envConfig from 'env-config';
+
+const origin = envConfig.ORIGIN;
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
-import { LayoutRowTemplate, LayoutCellTemplate } from './Layout';
-import Image from './Image';
+import _ from 'lodash';
 
-export {
-  LayoutRowTemplate,
-  LayoutCellTemplate,
-  Image,
-};
+// Config - Share environment variables between server and client
+
+const rootElementId = 'config';
+
+class SharedConfig {
+  constructor() {
+    this.desiredEnvVars = {};
+  }
+
+  isClient() {
+    return (typeof window !== 'undefined');
+  }
+  isServer() {
+    return (typeof process !== 'undefined');
+  }
+
+  registerEnvVar(varName, defaultValue) {
+    this.desiredEnvVars[varName] = {
+      name: varName,
+      defaultValue,
+    };
+  }
+
+  // extractVarsFromEnv tries to extract the desired values from the environemnt
+  extractVarsFromEnv() {
+    if (this.isServer()) {
+      // Iterate over desired env vars
+      _.each(this.desiredEnvVars, (envVar, varName) => {
+        let value = process.env[varName];
+
+        // fallback to default value if not found
+        value = value || envVar.defaultValue;
+
+        // Extend the config
+        this[varName] = value;
+      });
+    }
+  }
+
+  // helper to insert this env on the page
+  // i.e. the server should render the html page with this:
+  // <html><body><%- config.renderScriptTag() %></body></html>
+  renderScriptTag() {
+    return (`
+      <script id="${rootElementId}" type="application/json">
+        ${JSON.stringify(this).replace(/\//g, '\\/')}
+      </script>
+    `);
+  }
+
+  // in the browser, hydrate config as declared above
+  hydrateClient() {
+    if (this.isClient()) {
+      _.extend(this, JSON.parse(document.getElementById(rootElementId).textContent));
+    }
+  }
+}
+
+const envConfig = new SharedConfig();
+
+// parse the server-rendered script tag
+// NOTE: this line will only have effect when run on the client
+envConfig.hydrateClient();
+
+export default envConfig;
